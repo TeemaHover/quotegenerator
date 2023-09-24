a0fb2f0c9f19d7e047e975f57e17577451422d36
Teema <temuulenhover@gmail.com>
2023-09-25 01:01:47 +0800

    first commit

---
+++ app/components/quote.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface QuoteProps {
+  text: string;
+  author: string;
+}
+
+const Quote: React.FC<QuoteProps> = ({ text, author }) => {
+  return (
+    <div className="quote">
+      <p className='font-bold'>" {text} "</p>
+      <p className='mt-4'>- {author}</p>
+    </div>
+  );
+};
+
+export default Quote;
---
+++ app/components/quotegenerator.tsx
@@ -0,0 +1,70 @@
+"use client"
+import React, { useState, useEffect } from 'react';
+import Quote from './quote';
+
+const QuoteGenerator: React.FC = () => {
+
+    const url = 'https://api.quotable.io/random'
+    // const url = 'https://quotes-api8.p.rapidapi.com/quotes/random';
+    // const options = {
+    //     method: 'GET',
+    //     headers: {
+    //         'X-RapidAPI-Key': '474b5ca236mshe2492f368536e07p1721c0jsn63aba7dc079a',
+    //         'X-RapidAPI-Host': 'quotes-api8.p.rapidapi.com'
+    //     }
+    // };
+
+    // const callAPI = async () => {
+    //     try {
+    //         const res = await fetch(
+    //             url, options
+    //         );
+    //         const data = await res.json();
+    //         console.log(data);
+    //     } catch (err) {
+    //         console.log(err);
+    //     }
+    // };
+
+    const [quote, setQuote] = useState<{ content: string; author: string }>({
+        content: '',
+        author: '',
+    });
+
+
+    // useEffect(() => {
+    //     fetch(url, options)
+    //         .then((response) => response.json())
+    //         .then((data) => {
+    //             setQuote(data);
+    //         });
+    // }, []);
+
+    const getNewQuote = () => {
+
+        // fetch(url, options)
+        //     .then((response) => response.json())
+        //     .then((data) => {
+        //         setQuote(data);
+        //     });
+        // console.log(quote.quote)
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                setQuote(data)
+            })
+        console.log(quote.content)
+    };
+
+    return (
+        <div className="quote-generator flex justify-center items-center flex-col">
+            <button onClick={getNewQuote} className='text-blue-200 border-solid border-2 border-white rounded-md p-3 mb-5'>Get New Quote</button>
+            <div>
+                <Quote text={quote.content} author={quote.author} />
+            </div>
+
+        </div>
+    );
+};
+
+export default QuoteGenerator;
